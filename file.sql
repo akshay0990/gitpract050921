3047db937006805dd346ed32ece6164af3b02fc1
Akshay <a@a.com>
2021-09-11 16:09:13 +0530

    Changes added 1

--- file.sql
+++ file.sql
@@ -1,1 +1,29 @@
 hello world 1 2 3 4
+
+#include <stdio.h>
+
+int my_strlen(char *s)
+{
+   char *p = s;
+
+   while (*p)
+      ++p;
+
+   return (p - s);
+}
+
+int main(void)
+{
+   int i;
+   char *s[] =
+   {
+      "Git tutorials",
+      "WebMagic Informatica"
+   };
+
+   for (i = 0; i < 2; ++i)
+
+   printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+
+   return 0;
+}
